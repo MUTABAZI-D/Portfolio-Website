a4d901c6478ce6a4d604ce0e947370272794fd1d
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-12-09 19:17:54 +0200

    edited the footer for better experience

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Logo from "../../public/Logo.png";
 
 export const Footer = () => {
   return (
-    <footer className="footer border border-[#33353F] border-l-transparent border-r-transparent text-white border-b-transparent relative">
-      <div className="absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-36 w-36 md:h-44 md:w-44 blur-lg z-0 bottom-0 left-0" />
-      <div className="container px-12 py-6 flex justify-between">
+    <footer className="footer bg-[#121212] border-t border-[#33353F] text-white relative">
+      <div className="absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-32 w-36 md:h-36 md:w-44 blur-lg z-0 bottom-0 -left-6 md:-left-8" />
+      <div className="container px-6 py-6 flex justify-between items-center">
         <Image src={Logo} alt="Logo" width={50} height={50} />
         <p className="text-slate-600">All rights reserved.</p>
       </div>
